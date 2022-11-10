8fef9cdfafeb6dbdf1ae2cb634177e70281f0765
shubham-matoliya <105078266+shubham-matoliya@users.noreply.github.com>
2022-11-10 12:36:53 +0530

    added to homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Heading } from "@chakra-ui/react";
 
 function App() {
   return (
     <div className="App">
-    
+      <Heading>Home Page</Heading>
     </div>
   );
 }
